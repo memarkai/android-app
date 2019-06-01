550908c08717e29c03d8caad2a3f8ea683867ffe
Lucas Oliveira Maggi <lom@cin.ufpe.br>
2019-06-01 16:06:03 -0300

    Fixed compilation error.

--- src/app/tabs/calendar/calendar.page.ts
+++ src/app/tabs/calendar/calendar.page.ts
@@ -41,7 +41,6 @@ export class CalendarPage implements OnInit {
  
   ngOnInit() {
     this.resetEvent();
-    document.getElementsByClassName("addEvent_header_btn")[0].onclick = this.addEventHeaderBtnClick;
   }
  
   resetEvent() {
@@ -54,34 +53,6 @@ export class CalendarPage implements OnInit {
     };
   }
  
-  addEventHeaderBtnClick() {
-    if (document.getElementById("addEventPopup").style.opacity == "1") {
-      document.getElementById("addEventPopup").style.animationName = "hide";
-      document.getElementById("addEventPopup").style.animationDuration = "0.3s";
-      document.getElementById("addEventPopup").style.visibility = "hidden";
-      document.getElementById("addEventPopup").style.opacity = "0";
-      setTimeout(function(){
-        document.getElementById("addEventPopup").style.animationName = "";
-        document.getElementById("addEventPopup").style.animationDuration = "";
-      },300);
-
-      // document.getElementsByTagName("ion-content")[0].style.visibility = "visible";
-      document.getElementsByClassName("addEvent_header_btn")[0].style = "transform:rotate(0deg);";
-    } else {
-      document.getElementById("addEventPopup").style.animationName = "show";
-      document.getElementById("addEventPopup").style.animationDuration = "0.3s";
-      document.getElementById("addEventPopup").style.visibility = "visible";
-      document.getElementById("addEventPopup").style.opacity = "1";
-
-      setTimeout(function(){
-        document.getElementById("addEventPopup").style.animationName = "";
-        document.getElementById("addEventPopup").style.animationDuration = "";
-      },300);
-      // document.getElementsByTagName("ion-content")[0].style.visibility = "hidden";
-      document.getElementsByClassName("addEvent_header_btn")[0].style = "transform:rotate(45deg);";
-    }
-  }
-
   // Create the right event format and reload source
   addEvent() {
     let eventCopy = {
