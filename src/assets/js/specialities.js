7a33e75eec549782869e6ce3e16338322e972078
Lucas Oliveira Maggi <lom@cin.ufpe.br>
2019-06-30 11:46:34 -0300

    Separated semantic construction from page code.

---
+++ src/assets/js/specialities.js
@@ -0,0 +1,3 @@
+var specialities = [{name:"Odontologista", keywords:["Dente", "Boca", "Dentista", "Odonto", "Odontologia", "Odontologista"], icon: "assets/icon/Dente.png"},
+  {name:"Ginecologista", keywords:["Ginecologista", "Ginecologia"], icon: "assets/icon/Estetoscopio.png"},
+  {name:"Otorrino", keywords:["Ouvido", "Otorrino"], icon: "assets/icon/Estetoscopio.png"}];
