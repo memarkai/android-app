507d9b6a30968651a6311168fbb450cd5ad55288
Lucas Oliveira Maggi <lom@cin.ufpe.br>
2019-07-01 03:45:44 -0300

    Better integration with ionic, and angular. Now working with maps and ion-range.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+declare function initializeMaps(): any;
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -15,6 +17,7 @@ export class AppComponent {
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+    initializeMaps();
   }
 
   initializeApp() {
@@ -22,5 +25,8 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+  changedRange( event) {
+    console.log(event.detail.value);
   }
 }
--- src/assets/js/tutorial.js
+++ src/assets/js/tutorial.js
@@ -38,3 +38,86 @@ function setScreens() {
     screens = document.getElementsByClassName("tutorial")[0].getElementsByClassName("screen");
     previousScreen();
 }
+
+function scriptLoader(srcURL) {
+    var loaderScript = document.createElement("script");
+    loaderScript.src = srcURL;
+    document.body.append(loaderScript);
+}
+
+function initializeMaps() {
+    var srcURL = "https://maps.googleapis.com/maps/api/js?key="+THE_GOOGLE_MAPS_API_KEY+"&callback=initMap";
+    scriptLoader(srcURL);
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      //center: {lat: -8.0646842, lng: -34.8955189},
+      center: {lat:-8.0606528,lng:-34.8721819},
+      zoom : 12
+    });
+   destinationCircle = new google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: map.center,
+      draggable: false,
+      editable: false,
+      radius: 2000 // 2km
+    });
+    // map.onclick = function() {
+    //   console.log(destinationCircle.map);
+    //   if (destinationCircle.map == null) {
+    //     destinationCircle.setCenter(map.getCenter());
+    //     destinationCircle.setMap(map);
+    //   }
+    // }
+    map.bounds_changed = function(){
+      var bNE = map.getBounds().getNorthEast();
+      var bSW = map.getBounds().getSouthWest();
+      var bArr = [{lat:bNE.lat, lng:bNE.lng},
+                  {lat:bNE.lat, lng:bSW.lng},
+                  {lat:bSW.lat, lng:bSW.lng},
+                  {lat:bSW.lat, lng:bNE.lng}];
+      var area = google.maps.geometry.spherical.computeArea(bArr);
+      var cArea = Math.PI*destinationCircle.radius*destinationCircle.radius;
+      destinationCircle.setCenter(map.getCenter());
+      // console.log(cArea/area);
+      /*if (cArea/area < 0.3) {
+        destinationCircle.
+      }*/
+    }
+    // infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       var pos = {
+//         lat: position.coords.latitude,
+//         lng: position.coords.longitude
+//       };
+
+//       infoWindow.setPosition(pos);
+//       infoWindow.setContent('Location found.');
+//       infoWindow.open(map);
+//       map.setCenter(pos);
+//     }, function() {
+//       handleLocationError(true, infoWindow, map.getCenter());
+//     });
+//   } else {
+//     // Browser doesn't support Geolocation
+//     handleLocationError(false, infoWindow, map.getCenter());
+//   }
+// }
+
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(browserHasGeolocation ?
+//                         'Error: The Geolocation service failed.' :
+//                         'Error: Your browser doesn\'t support geolocation.');
+//   infoWindow.open(map);
+// }
+  }
